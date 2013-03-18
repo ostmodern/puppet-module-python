1d503933e9f6dfbd177c0bb4615b930e312e5bac
Lx Yu <lixinfish@gmail.com>
2013-03-18 15:32:25 +0800

    refine pip install function
    remove package param as it's useless

--- manifests/pip/install.pp
+++ manifests/pip/install.pp
@@ -1,31 +1,34 @@
-define python::pip::install($package, $venv, $ensure=present,
-                            $owner=undef, $group=undef) {
-
+define python::pip::install (
+  $venv,
+  $ensure = present,
+  $owner  = undef,
+  $group  = undef
+) {
   # Match against whole line if we provide a given version:
-  $grep_regex = $package ? {
-    /==/ => "^${package}\$",
-    default => "^${package}==",
+  $grep_regex = $name ? {
+    /==/    => "^${name}\$",
+    default => "^${name}==",
   }
 
   Exec {
-    user => $owner,
+    user  => $owner,
     group => $group,
-    cwd => "/tmp",
+    cwd   => "/tmp",
   }
 
   if $ensure == 'present' {
     exec { "pip install $name":
-      command => "$venv/bin/pip install $package",
-      unless => "$venv/bin/pip freeze | grep -e $grep_regex"
+      command => "$venv/bin/pip install $name",
+      unless  => "$venv/bin/pip freeze | grep -e $grep_regex"
     }
   } elsif $ensure == 'latest' {
     exec { "pip install $name":
-      command => "$venv/bin/pip install -U $package",
+      command => "$venv/bin/pip install -U $name",
     }
   } else {
     exec { "pip install $name":
-      command => "$venv/bin/pip uninstall $package",
-      onlyif => "$venv/bin/pip freeze | grep -e $grep_regex"
+      command => "$venv/bin/pip uninstall $name",
+      onlyif  => "$venv/bin/pip freeze | grep -e $grep_regex"
     }
   }
 }
